81433ba1a9f09a8c45db5eec0e009dfdbef807fc
RolePoloGame <78389182+RolePoloGame@users.noreply.github.com>
2024-08-11 14:24:38 +0200

    AgentUI display

---
+++ RTS_PROJECT/Assets/RTSProject/Code/UI/Agents/AgentInfoUIView.cs
@@ -0,0 +1,83 @@
+using RTS.Core;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace RTS.UI
+{
+    public class AgentInfoUIView : UIView
+    {
+        [SerializeField] private TextMeshProUGUI amountTMP;
+
+
+        [SerializeField] private Dictionary<UniqueID, AgentUIElement> infoList = new();
+        [SerializeField] private AgentUIElement elementPrefab;
+        [SerializeField] private Transform root;
+
+
+        private ObjectPool<AgentUIElement> pool;
+        private IAgentService service;
+
+        private void OnEnable()
+        {
+            pool = new ObjectPool<AgentUIElement>(CreateElement);
+            service = ServiceManager.Instance.Get<IAgentService>();
+            if (service == null)
+            {
+                Debug.LogError($"There is no service of type {nameof(IAgentService)}");
+                enabled = false;
+            }
+            service.OnAgentSpawned += OnAgentSpawned;
+            service.OnAgentRemoved += OnAgentRemoved;
+            service.OnAgentArrived += OnAgentArrived;
+        }
+
+        public void RequestSpawn()
+        {
+            if (service == null) return;
+            service.RequestSpawnAgent();
+        }
+
+        private void OnAgentSpawned(UniqueID id)
+        {
+            SpawnNotice($"[{id}] was added");
+            UpdateLabel();
+        }
+
+        private void UpdateLabel() => amountTMP.SetText(service.AgentCount.ToString());
+
+        private void OnAgentArrived(UniqueID id) => SpawnNotice($"[{id}] arrived at destination");
+        private void OnAgentRemoved(UniqueID id)
+        {
+            SpawnNotice($"[{id}] was removed");
+            UpdateLabel();
+        }
+
+        private void SpawnNotice(string message)
+        {
+            var item = pool.Get();
+            item.gameObject.SetActive(true);
+            item.Show(message);
+            item.OnAgentFadeComplete += Item_OnAgentFadeComplete;
+        }
+
+        private void Item_OnAgentFadeComplete(AgentUIElement obj)
+        {
+            pool.Release(obj);
+            obj.gameObject.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            service.OnAgentSpawned -= OnAgentSpawned;
+            service.OnAgentRemoved -= OnAgentSpawned;
+        }
+
+
+        private AgentUIElement CreateElement()
+        {
+            return Instantiate(elementPrefab, root);
+        }
+    }
+}
